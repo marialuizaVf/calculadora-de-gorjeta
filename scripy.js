76c2b593f82538b0183b53fef007ef32792ac463
Maria Luiza <maluv3106@gmail.com>
2024-03-31 18:56:24 -0300

    sexto commit do JS E AGORA SIM PROJETO FINALIZADO

--- scripy.js
+++ scripy.js
@@ -1,61 +1,73 @@
-let conta=0
-let pessoas=0
-let porcentagem=0
+let conta = 0
+let pessoas = 0
+let porcentagem = 0
 
-const contaInput=document.querySelector("#conta")
+const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
 
-function receberValorConta(evento){
-    conta=Number(evento.target.value)
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
     calcular()
 }
 
-const pessoasInput=document.querySelector("#pessoas")
-pessoasInput.addEventListener("input", receberQuantidadedePessoas)
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
 
-function receberQuantidadedePessoas(evento){
-    const paragrafoErro= document.querySelector(".pessoas #erro")
-    const divErro= document.querySelector(".pessoas .input-box")
+function receberQuantidadePessoas(evento) {
+    
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
 
-    if(evento.target.value ==="0"){
-        paragrafoErro.style.display="block"
-        divErro.setAttribute("id","erro-div")
+    if(evento.target.value === "0") {
+        paragrafoErro.style.display = "block"
+        divErro.setAttribute("id", "erro-div")
     } else {
-        paragrafoErro.style.display="none"
-        divErro.setAttribute("id","")
-        pessoas= Number(evento.target.value)
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
+
     }
+
     calcular()
 }
 
-const botoesGorjetas= document.querySelectorAll(".gorjetas input[type='button']")
-botoesGorjetas.forEach(botao => {
-    botao.addEventListener("click", recerberPorcentagem)
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem)
 })
-function recerberPorcentagem(evento){
-    botoesGorjetas.forEach(botao => {
+
+function receberPorcentagem(evento) {
+    botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
-        if(botao.value === evento.target.value){
+
+        if(botao.value === evento.target.value) {
             botao.classList.add("botao-ativo")
         }
     })
-    if(evento.target.value !==""){
-        porcentagem=parseFloat(evento.target.value) / 100
-    } else {
-        porcentagem=0
-    }
-    calcular()
-}
-const gorjetaInput=document.querySelector("#outro")
-gorjetaInput.addEventListener("input", recerberPorcentagem)
 
-function calcular(){
-    if(conta !==0 && porcentagem !== 0 && pessoas !==0){
-        const strongGorjetaTotal= document.querySelector(".gorjeta-total>strong")
-        strongGorjetaTotal.innerHTML=`R$ ${(conta * porcentagem /pessoas).toFixed(2)}`
-        const strongTotal= document.querySelector(".total>strong")
-        strongTotal.innerHTMLl=`R$ ${(conta+(conta*porcentagem)/pessoas).toFixed(2)}`
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
     }
+
+    calcular()
+
+}
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
+
+function calcular() {
+    if(conta !== 0 && porcentagem !== 0 && pessoas !== 0 ) {
+        const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
+        strongGorjetaTotal.innerHTML = `R$ ${(conta * porcentagem / pessoas).toFixed(2)}`
+
+        const strongTotal = document.querySelector(".total > strong")
+        strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}`
+    }
+
 }
 
 const botaoLimpar = document.querySelector(".resultados button")
@@ -64,7 +76,7 @@ botaoLimpar.addEventListener("click", limpar)
 function limpar() {
     contaInput.value = ""
 
-    botoesGorjetas.forEach(botao => {
+    botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
     })
 
